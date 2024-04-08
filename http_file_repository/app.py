6aa05cf8186c9a1a6ac284bbacc377d461ff415f
Alexandr <alexandr.panteleev2000@gmail.com>
2024-04-08 13:49:00 +0300

    working with users

--- http_file_repository/app.py
+++ http_file_repository/app.py
@@ -1,28 +1,44 @@
 from flask import Flask, request, jsonify, Response, send_file
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_httpauth import HTTPBasicAuth
 
 from file_repository import FileRepository
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 app.config["UPLOAD_FOLDER"] = "store"
 
+users = {
+    "alexandr": generate_password_hash("123"),
+    "julia": generate_password_hash("321")
+}
+
+
+@auth.verify_password  # type: ignore
+def verify_password(username: str, password: str) -> str | None:
+    if username in users and \
+            check_password_hash(users.get(username), password):  # type: ignore
+        return username
+
 
 @app.route("/upload", methods=["POST"])
+@auth.login_required  # type: ignore
 def upload_file() -> Response:
     repository: FileRepository = FileRepository(app.config["UPLOAD_FOLDER"])  # type: ignore
     file: FileStorage = request.files["file"]
-    file_data: bytes = file.read()
-    file_hash: str = repository.save(file_data)
+    file_hash: str = repository.save(auth.current_user(), file)
     return jsonify({"file_hash": file_hash})
 
 
 @app.route("/delete", methods=["POST"])
+@auth.login_required  # type: ignore
 def delete_file() -> Response:
     repository: FileRepository = FileRepository(app.config["UPLOAD_FOLDER"])  # type: ignore
     file_hash: str = request.form["file_hash"]
-    repository.delete(filename=file_hash)
+    repository.delete(username=auth.current_user(), filename=secure_filename(file_hash))
     return jsonify({"status": "deleted"})
 
 
--- http_file_repository/file_repository.py
+++ http_file_repository/file_repository.py
@@ -1,11 +1,20 @@
+import io
 from pathlib import Path
 from abc import ABC, abstractmethod
-from hashlib import sha256
+from hashlib import sha512, file_digest
+from hmac import HMAC
+
+from werkzeug.datastructures import FileStorage
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from hashlib import _Hash  # type: ignore
 
 
 class AbstractFileRepository(ABC):
     @abstractmethod
-    def save(self, file_data: bytes) -> str: ...  
+    def save(self, username: str, file: FileStorage) -> str: ...  
 
     @abstractmethod
     def get(self, filename: str) -> str: ...  
@@ -15,13 +24,18 @@ class AbstractFileRepository(ABC):
 
 
 class FileRepository(AbstractFileRepository):
+    _bufsize = 262144
+
     def __init__(self, base_dir: str) -> None:
         self.base_dir = base_dir
 
-    def __compute_hash(self, data: bytes) -> str:
-        sha256_hash = sha256(data)
+    def __compute_hash(self, username: str, file: FileStorage) -> "_Hash":
+        encoded_username: bytes = bytes(username, "UTF-8")
+        mac = HMAC(encoded_username, digestmod=sha512)
+        file_content = io.BytesIO(file.read())
+        digest = file_digest(file_content, lambda: mac)  # type: ignore
 
-        return sha256_hash.hexdigest()
+        return digest
 
     def __mkdir(self, path: Path) -> None:
         Path.mkdir(path, parents=True, exist_ok=True)
@@ -29,22 +43,30 @@ class FileRepository(AbstractFileRepository):
     def __mkfile(self, path: Path) -> None:
         Path.touch(path, exist_ok=False)
 
-    def save(self, file_data: bytes) -> str:
-        file_hash = self.__compute_hash(file_data)
-        file_dir = Path(self.base_dir, file_hash[:2])
-        file_path = Path(file_dir, file_hash)
+    def save(self, username: str, file: FileStorage) -> str:
+        file_hash = self.__compute_hash(username, file)
+        file_hexdigest = file_hash.hexdigest()
+        file_dir = Path(self.base_dir, file_hexdigest[:2])
+        file_path = Path(file_dir, file_hexdigest)
 
         self.__mkdir(file_dir)
         self.__mkfile(file_path)
 
-        with open(file_path, "wb") as file:
-            file.write(file_data)
+        with open(file_path, "wb") as local_file:
+            block: bytes = file.read(self._bufsize)
+            while block:
+                local_file.write(block)
+                block: bytes = file.read(self._bufsize)
 
-        return file_hash
+        return file_hexdigest
 
     def get(self, filename: str) -> str:
         return Path(self.base_dir, filename[:2], filename).__str__()
 
-    def delete(self, filename: str) -> None:
+    def delete(self, *, username: str, filename: str) -> None:
+        # encoded_username: bytes = bytes(username, "UTF-8")
+        # mac = HMAC(encoded_username, digestmod=sha512)
+        # local_digest = file_digest()
+
         file_path = Path(self.base_dir, filename[:2], filename)
         Path.unlink(file_path)
